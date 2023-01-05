fc3bd2ee8a1eb448c39ed7eb4f6e092254dac76a
carlos <carlos@MacBook-Pro.local>
2023-01-05 13:34:38 -0500

    changes carlos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   api: {
-    baseUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/v1/',
-    baseAuthUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/',
-    baseProfileUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/profile',
+    baseUrl: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/v1/',
+    baseUrl2: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/',
+    baseAuthUrl: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/auth/',
+    baseProfileUrl: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/auth/profile',
     bankImage: 'https://imagesdyner.s3.us-east-2.amazonaws.com',
     linkImage: 'https://img-productos-comercios-arianpay.s3.us-east-2.amazonaws.com',
-    gatewayUrl: 'https://www.develop-dyner24.online/dynergateway/public/'
+    gatewayUrl: 'https://www.dashboard-dyner24.com/dynergateway/public/'
   },
 };
 
