3a9fb7e673b12b92295ec6163a9817d758690937
Edwar Forero <eafp.91@gmail.com>
2023-01-26 20:43:29 -0500

    add badged with notifications number on notification panel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
     production: false,
     api: {
-        baseUrl: ' https://www.develop-dyner24.online/dyner-api-mobile/public/api/v1/',
-        baseUrl2: ' https://www.develop-dyner24.online/dyner-api-mobile/public/api/',
-        baseAuthUrl: ' https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/',
-        baseProfileUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/profile',
+        baseUrl: ' http://52.44.5.11:2100/dyner-api-mobile/public/api/v1/',
+        baseUrl2: ' http://52.44.5.11:2100/dyner-api-mobile/public/api/',
+        baseAuthUrl: ' http://52.44.5.11:2100/dyner-api-mobile/public/api/auth/',
+        baseProfileUrl: 'http://52.44.5.11:2100/dyner-api-mobile/public/api/auth/profile',
         bankImage: 'https://imagesdyner.s3.us-east-2.amazonaws.com',
         linkImage: 'https://img-productos-comercios-arianpay.s3.us-east-2.amazonaws.com',
         gatewayUrl: 'http://52.44.5.11:1400/dynergateway/public/'
