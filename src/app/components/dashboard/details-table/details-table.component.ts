9d7aae0a2ac16cc51adf753fbd0abbb6d61b5a52
Edwar Forero <eafp.91@gmail.com>
2023-02-21 17:43:44 -0500

    se cambian estilos de link 'ver detalle'

--- src/app/components/dashboard/details-table/details-table.component.ts
+++ src/app/components/dashboard/details-table/details-table.component.ts
@@ -12,7 +12,7 @@ import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-details-table',
   templateUrl: './details-table.component.html',
-  styleUrls: ['./details-table.component.scss'],
+  styleUrls: ['./details-table.component.scss'], 
 })
 export class DetailsTableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
