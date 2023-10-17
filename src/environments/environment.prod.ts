22a89cfd2513ed415eb1bbda36b27f0ba2037328
Carlos Arriero <desarrollo@dyner24.com>
2023-10-17 13:44:02 -0500

    quit validate transfer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,19 +6,19 @@ export const environment = {
     //   baseUrl2: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/',
     //   baseAuthUrl: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/auth/',
     //   baseProfileUrl: 'https://www.dashboard-dyner24.com/dyner-api-mobile/public/api/auth/profile',
-    //   bankImage: 'https://imagesdyner.s3.us-east-2.amazonaws.com',
-    //   linkImage: 'https://img-productos-comercios-dyner-prod.s3.us-east-2.amazonaws.com',
+    //   bankImage: 'https://imagesdynerv1.s3.us-east-2.amazonaws.com',
+    //   linkImage: 'https://img-productos-comercios-dyner-prod-v1.s3.us-east-2.amazonaws.com',
     //   gatewayUrl: 'https://www.dashboard-dyner24.com/dynergateway/public/'
     // },
     // URL'S TO DEV QA ENVIROMENT //
     api: {
-        baseUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/v1/',
-        baseUrl2: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/',
-        baseAuthUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/',
-        baseProfileUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/profile',
-        bankImage: 'https://imagesdyner.s3.us-east-2.amazonaws.com',
+        baseUrl: 'https://www.develop-dyner24.cloud/dyner-api-mobile/public/api/v1/',
+        baseUrl2: 'https://www.develop-dyner24.cloud/dyner-api-mobile/public/api/',
+        baseAuthUrl: 'https://www.develop-dyner24.cloud/dyner-api-mobile/public/api/auth/',
+        baseProfileUrl: 'https://www.develop-dyner24.cloud/dyner-api-mobile/public/api/auth/profile',
+        bankImage: 'https://imagesdynerv2.s3.us-east-2.amazonaws.com',
         linkImage: 'https://img-productos-comercios-arianpay.s3.us-east-2.amazonaws.com',
-        gatewayUrl: 'https://www.develop-dyner24.online/dynergateway/public/'
+        gatewayUrl: 'https://www.develop-dyner24.cloud/dynergateway/public/'
     },
 };
 
