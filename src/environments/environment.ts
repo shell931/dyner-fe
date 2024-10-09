9515382505b9f0c38b434b7b63f6077e5016266e
developers-gateway-pro <165091308+developers-gateway-pro@users.noreply.github.com>
2024-10-09 14:22:11 -0500

    changes front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
         baseUrl2: ' https://www.develop-dyner24.online/dyner-api-mobile/public/api/',
         baseAuthUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/',
         baseProfileUrl: 'https://www.develop-dyner24.online/dyner-api-mobile/public/api/auth/profile',
-        bankImage: 'https://imagesd24.s3.us-east-2.amazonaws.com',
-        linkImage: 'https://img-productos-comercios.s3.us-east-2.amazonaws.com',
+        bankImage: 'https://imagesd24-pro.s3.us-east-1.amazonaws.com',
+        linkImage: 'https://img-productos-comercios-pro.s3.us-east-1.amazonaws.com',
         gatewayUrl: 'http://52.44.5.11:1400/dynergateway/public/'
     },
 };
